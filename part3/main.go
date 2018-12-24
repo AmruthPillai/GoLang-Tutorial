5089d4781cbbeb466959919f97c7be2cd095f3b8
Amruth Pillai <im.amruth@gmail.com>
2018-12-24 12:55:51 +0530

    add, concat functions

---
+++ part3/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func add(x, y float64) float64 {
+	return x + y
+}
+
+func concat(a, b string) (string, string) {
+	return a, b
+}
+
+func main() {
+	// var num1 float64 = 5.6
+	// var num2 float64 = 9.5
+	// var num1, num2 float64 = 5.6, 9.5
+
+	// Defaults to float64
+	num1, num2 := 5.6, 9.5
+	fmt.Println(add(num1, num2))
+
+	w1, w2 := "Hey,", "there!"
+	fmt.Println(concat(w1, w2))
+
+	// Type Conversion
+	var a int = 62
+	var b float64 = float64(a)
+	fmt.Println("Value of B:", b)
+
+	// Type Inference, Value Copy
+	x := a
+	fmt.Println("Value of x:", x)
+}
