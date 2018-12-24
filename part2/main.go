1c762024a488c2bf40b90079daa578f816fa3c0c
Amruth Pillai <im.amruth@gmail.com>
2018-12-24 12:43:48 +0530

    part 2

---
+++ part2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("The square root of 4 is", math.Sqrt(4))
+	fmt.Println("A number from 1 to 100:", rand.Intn(100))
+}
