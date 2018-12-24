6e6a87675bc792f027fb8ba5c4724a9b19f2e68c
Amruth Pillai <im.amruth@gmail.com>
2018-12-24 14:35:26 +0530

    part13 - building web apps

---
+++ part13 - building-web-app/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+)
+
+// SitemapIndex - Sitemap Index Structure
+type SitemapIndex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+func main() {
+	var s SitemapIndex
+	var n News
+
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+
+	for _, Location := range s.Locations {
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+	}
+}
